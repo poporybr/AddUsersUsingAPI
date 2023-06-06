c1efd8e2f58a56cdf64ff2f88388641805fe164f
Eduardo Marques <112179103+poporybr@users.noreply.github.com>
2023-06-05 22:06:39 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,60 +1,77 @@
-async function buscarCep(cep){
-    const cepUser = cep
-    try{
-        const response = await fetch(`https://viacep.com.br/ws/${cepUser}/json/`)
-        const endereço = await response.json();
+// const form = document.getElementById('formularioUser')
+// form.addEventListener('submit', function(event) {
+//   event.preventDefault();
+// });
 
-        if(endereço.erro){
-            alert(`O CEP ${cepUser}, é Invalído`);
-        }else{
-            const resultado = {
-                cep: endereço.cep,
-                estado: endereço.uf,
-                cidade: endereço.localidade,
-                bairro: endereço.bairro
-            }
-            console.log(resultado);
-            return resultado;
-        }
-    }catch (error) {
-        console.log('Ocorreu um erro:', error);
-      }
+async function buscarCep(cep) {
+  const cepSemTracos = cep.replace(/-/g, '');
+
+  const cepValido = /^\d{8}$/.test(cepSemTracos);
+  
+  if (!cepValido) {
+    alert(`O CEP ${cep} é inválido`);
+    return;
+  }
+
+  try {
+    const response = await fetch(`https://viacep.com.br/ws/${cepSemTracos}/json/`);
+    const endereco = await response.json();
+
+    if (endereco.erro) {
+      alert(`O CEP ${cep} é inválido`);
+    } else {
+      const resultado = {
+        cep: endereco.cep,
+        estado: endereco.uf,
+        cidade: endereco.localidade,
+        bairro: endereco.bairro,
+        logradouro: endereco.logradouro
+      };
+
+      console.log(resultado);
+      return resultado;
+    }
+  } catch (error) {
+    console.log('Ocorreu um erro:', error);
+  }
 }
 
 
+
 async function addUser(){
-    let nameUser = document.getElementById('nameUser').value
-    let emailUser = document.getElementById('emailUser').value
-    let cepUser = document.getElementById('cepUser').value
+  let nameUser = document.getElementById('nameUser').value
+  let emailUser = document.getElementById('emailUser').value
+  let cepUser = document.getElementById('cepUser').value
     
-    try {
-        const enderecoUser = await buscarCep(cepUser);
-        console.log(enderecoUser);
-    
-        const usuario = {
-          nome: nameUser,
-          email: emailUser,
-          cep: enderecoUser.cep,
-          estado: enderecoUser.estado,
-          cidade: enderecoUser.cidade,
-          bairro: enderecoUser.bairro
-        };
-    
-        console.log(usuario);
-    
-        let usuarios = localStorage.getItem('usuarios');
-        if (usuarios) {
-          usuarios = JSON.parse(usuarios);
-          usuarios.push(usuario);
-        } else {
-          usuarios = [usuario];
-        }
-    
-        localStorage.setItem('usuarios', JSON.stringify(usuarios));
-        showUsers();
-      } catch (error) {
-        console.log('Ocorreu um erro:', error);
+  try {
+      const enderecoUser = await buscarCep(cepUser);
+      console.log(enderecoUser);
+  
+      const usuario = {
+        nome: nameUser,
+        email: emailUser,
+        cep: enderecoUser.cep,
+        estado: enderecoUser.estado,
+        cidade: enderecoUser.cidade,
+        bairro: enderecoUser.bairro,
+        logradouro: enderecoUser.logradouro
+      };
+  
+      console.log(usuario);
+  
+      let usuarios = localStorage.getItem('usuarios');
+      if (usuarios) {
+        usuarios = JSON.parse(usuarios);
+        usuarios.push(usuario);
+      } else {
+        usuarios = [usuario];
       }
+  
+      localStorage.setItem('usuarios', JSON.stringify(usuarios));
+    } catch (error) {
+      console.log('Ocorreu um erro:', error);
+    }
+
 }
 
 function showUsers() {
@@ -63,7 +80,7 @@ function showUsers() {
     let list = document.getElementById('listUsers')
     arrayUsers.forEach(user => {
       const itemLista = document.createElement('li')
-      itemLista.textContent = `Nome: ${user.nome}, Email: ${user.email}, CEP: ${user.cep}, Estado: ${user.estado}, Cidade: ${user.cidade}, Bairro: ${user.bairro}`
+      itemLista.textContent = `Nome: ${user.nome}, Email: ${user.email}, CEP: ${user.cep}, Logradouro: ${user.logradouro}, Estado: ${user.estado}, Cidade: ${user.cidade}, Bairro: ${user.bairro}`
       list.append(itemLista)
     });
 }
